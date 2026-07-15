8de611d1cd3b5f73606956848d38efcd010601fd
IlyaBogdanovich <hyperboreanwayfarer@gmail.com>
2026-07-15 17:05:38 +0300

    Add files via upload

---
+++ hw/hw10/IlyaBogdanovich/Task2.py
@@ -0,0 +1,13 @@
+class Human:
+    def __init__(self, name):
+        self.names = name
+
+    def welcome_message(self):
+            print(f"Welcome, {self.name}!")
+
+    def species(cls):
+        return "Homosapiens"
+    
+    @staticmethod
+    def arbitrary_message():
+        print("I love Python")
