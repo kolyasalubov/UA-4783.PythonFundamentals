7c9b65a9b3a1dbad7f80d17ca8c4dbbb462bac78
bohdanatamaniuk-stack <bohdan.atamaniuk@icloud.com>
2026-07-20 23:44:40 +0300

    Second idea

--- hw/hw08/BohdanAtamaniuk/hw08_02.py
+++ hw/hw08/BohdanAtamaniuk/hw08_02.py
@@ -2,8 +2,22 @@ import re
 
 
 def validate_password(password):
-    valid = re.findall(r'[a-z][A-Z][1-9][^0-9]', password)
-    return valid
+    if 6 < len(password) < 16:
+        if re.search(r'[a-z]', password) != None:
+            if re.search(r'[A_Z]', password) != None:
+                if re.search(r'\d', password) != None: 
+                    if re.search(r'^[0-9]', password):
+                        return ("Ur password is saved")
+                    else:
+                        return("Ur password is unvalid")
+                else:
+                    return("Ur password is unvalid")
+            else:
+                return("Ur password is unvalid")
+        else:
+            return("Ur password is unvalid")
+    else:
+        return("Ur password is unvalid")
 
 password = input("Enter ur password: ")
 print(validate_password(password))
