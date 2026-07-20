6d5007fbc1f110b1e292d80b0845642d928caa6c
bohdanatamaniuk-stack <bohdan.atamaniuk@icloud.com>
2026-07-21 00:17:47 +0300

    add file hw08_03 and realize ask of user about what script need calculate git status exit

--- hw/hw08/BohdanAtamaniuk/hw08_03.py
+++ hw/hw08/BohdanAtamaniuk/hw08_03.py
@@ -1,7 +1,15 @@
 import area
-
-print(area.circle_area(10))
-
-print(area.rectangle_area(10, 5))
-
-print(area.triangle_area(10, 5))
+calculate = input('Enter what area do u want calculate: (r(rectangle), c(circle), t(triangle)\n')
+if calculate.lower() == 'c':
+    radius = float(input('Enter a radius of the circle: \n'))
+    print(area.circle_area(radius))
+elif calculate.lower() == 'r':
+    a = float(input('Enter a lenght of the rectangle: \n'))
+    b = float(input('Enter a width of the rectangle: \n'))
+    print(area.rectangle_area(a, b))
+elif calculate.lower() == 't': 
+    a = float(input('Enter a lenght of the triangle: \n'))
+    b = float(input("Enter a height of the triangle: \n"))
+    print(area.triangle_area(a, b))
+else:
+    print("We dont have this option")
