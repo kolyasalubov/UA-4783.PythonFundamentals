efd4478383f1988eed3ec78633b3d204d289d7d9
bohdanatamaniuk-stack <bohdan.atamaniuk@icloud.com>
2026-07-20 23:52:46 +0300

    Task is done

--- hw/hw08/BohdanAtamaniuk/hw08_02.py
+++ hw/hw08/BohdanAtamaniuk/hw08_02.py
@@ -2,11 +2,11 @@ import re
 
 
 def validate_password(password):
-    if 6 < len(password) < 16:
+    if 6 <= len(password) <= 16:
         if re.search(r'[a-z]', password) != None:
-            if re.search(r'[A_Z]', password) != None:
+            if re.search(r'[A-Z]', password) != None:
                 if re.search(r'\d', password) != None: 
-                    if re.search(r'^[0-9]', password):
+                    if re.search(r'[$#@]', password):
                         return ("Ur password is saved")
                     else:
                         return("Ur password is unvalid")
@@ -19,5 +19,11 @@ def validate_password(password):
     else:
         return("Ur password is unvalid")
 
-password = input("Enter ur password: ")
+password = input('''(Ur password may be:
+At least 1 letter between [a-z] and 1 letter between [A-Z].
+At least 1 number between [0-9].
+At least 1 character from [$#@].
+Minimum length 6 characters.
+Maximum length 16 characters.)
+Enter ur password: ''')
 print(validate_password(password))
