a5bc03252f8353a93d69fc6278f87d481aeecb30
Matviy Fedychkanych <matviy.fedychkanych@gmail.com>
2026-07-22 15:58:47 +0300

    Added hw 10 optional task1

--- hw/hw06/hw7/MatviyFedychkanych/task1.py
+++ hw/hw06/hw7/MatviyFedychkanych/task1.py
@@ -1,12 +1,13 @@
-def larg_number(x,y) -> float:
+def larg_number(x,y):
     """
     Ця функція визначає, яке з двох вказаних чисел є більшим
     
     Аргументи:
-    x(int, float)
-    y(int, float)
+    x(int) - перше число
+    y(int) - друге число
     
-    
+    Повертає:
+    int: Число x або число y
     """
     if x > y:
         return x
